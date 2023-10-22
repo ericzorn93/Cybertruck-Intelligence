03a21ef653d1664ce2feb269c39b0cb117c2cdc7
Eric Zorn <zornwebdev@gmail.com>
2023-10-22 17:27:41 -0400

    fix: logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,25 +8,18 @@ import {
   NavbarItem,
 } from "@nextui-org/navbar";
 import { Link } from "@nextui-org/link";
-import { useWindowSize } from "usehooks-ts";
-
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { FacebookIcon, GithubIcon, CybertruckLogo } from "@/components/icons";
 
 export const Navbar = () => {
-  const windowSize = useWindowSize();
-  const isSmallWindow = windowSize.width < 600;
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <CybertruckLogo width={isSmallWindow ? 80 : 100} />
-            <p className="font-bold text-inherit text-xl">
-              Cybertruck Intelligence
-            </p>
+            <CybertruckLogo width={80} />
+            <p className="font-bold text-inherit text-xl">Intelligence</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
