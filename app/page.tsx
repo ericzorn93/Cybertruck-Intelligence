7ec05b44da4f3a6cbeefe3d5f3fafb03dc6a1241
Eric Zorn <zornwebdev@gmail.com>
2023-10-22 18:01:56 -0400

    feat: begin adding vin number

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import { Vin } from "@/components/vin";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       Tesla Cybertruck Details
+      <Vin />
     </section>
   );
 }
---
+++ components/vin.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Input } from "@nextui-org/input";
+import { useState } from "react";
+
+export const Vin = () => {
+  const [vinNumber, setVinNumber] = useState<string>("");
+
+  return (
+    <Input
+      variant="bordered"
+      label="Please Enter Your VIN Number"
+      description="Find and Validate Your Cybertruck Vin Number"
+      labelPlacement="inside"
+      fullWidth={false}
+      size="lg"
+      radius="md"
+      onChange={(e) => setVinNumber(e.target.value)}
+      value={vinNumber}
+    />
+  );
+};
