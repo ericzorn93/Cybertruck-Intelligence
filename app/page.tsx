8b7151a1d97292e5efaca2592ea2b06ba21b2501
Eric Zorn <zornwebdev@gmail.com>
2023-10-23 22:52:41 -0400

    feat: adding vin identifier document and link

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { ArticleGrid } from "@/components/article-grid";
 import { VinNumberForm } from "@/components/vin-number";
 import { getCybertruckOwnersClubArticles } from "@/utils/getArticles";
 import { Divider } from "@nextui-org/divider";
+import { Link } from "@nextui-org/link";
 
 export default async function Home() {
   const articleData = await getCybertruckOwnersClubArticles();
@@ -16,7 +17,16 @@ export default async function Home() {
         <br />
         <Divider />
         <br />
-        <p className="my-3">VIN Number Validator</p>
+        <p className="my-3">
+          VIN Number Validator -{" "}
+          <Link
+            href="/vin-identifier.jpeg"
+            target="_blank"
+            about="VIN Number Decoder Document"
+          >
+            VIN Identifier Reference Document
+          </Link>
+        </p>
         <VinNumberForm />
       </div>
     </section>
