da95d39abd6f66b4bb03ac73aa2a5d5f132cf1c4
Eric Zorn <zornwebdev@gmail.com>
2023-10-23 22:28:17 -0400

    feat: adding grid complete (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { ArticleGrid } from "@/components/article-grid";
 import { VinNumberForm } from "@/components/vin-number";
+import { getCybertruckOwnersClubArticles } from "@/utils/getArticles";
+import { Divider } from "@nextui-org/divider";
 
-export default function Home() {
+export default async function Home() {
+  const articleData = await getCybertruckOwnersClubArticles();
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-4 py-5 md:py-10">
       <div className="w-full">
+        <Divider />
+        <br />
+        <p className="my-3">Latest Articles - Cybertruck Owners Club</p>
+        <ArticleGrid articleData={articleData} />
+        <br />
+        <Divider />
+        <br />
         <p className="my-3">VIN Number Validator</p>
         <VinNumberForm />
       </div>
