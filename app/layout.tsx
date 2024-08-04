807c1e00fadfee81aee3022cc4785a84be4c8b98
Eric Zorn <zornwebdev@gmail.com>
2024-08-04 14:17:52 -0400

    fix: types

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,13 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers
+          themeProps={{
+            attribute: "class",
+            defaultTheme: "dark",
+            children: [],
+          }}
+        >
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-8xl px-6 flex-grow">
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,21 @@
 "use client";
 
+import { type FC, type PropsWithChildren } from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 
 export interface ProvidersProps {
-  children: React.ReactNode;
   themeProps?: ThemeProviderProps;
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export const Providers: FC<PropsWithChildren<ProvidersProps>> = ({
+  themeProps,
+  children,
+}) => {
   return (
     <NextUIProvider>
       <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
     </NextUIProvider>
   );
-}
+};
